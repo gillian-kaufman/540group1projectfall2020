690f3a3338501ce6b2a929dbadeaf40cd23ab7ae
auzman466 <auzman446@gmail.com>
2020-10-06 12:40:37 -0400

    First Commit

---
+++ SoftwareEngineeringSilkRoad/Models/ProductReview.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftwareEngineeringSilkRoad.Models
+{
+    public class ProductReview
+    {
+        [Key]
+        public int ProductReviewID { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public string ReviewDetail { get; set; }
+    }
+}
