88ad2f51b5d8c28dff03a25b0bc020515e7dde32
auzman466 <auzman446@gmail.com>
2020-10-06 12:15:20 -0400

    First commit

---
+++ SoftwareEngineeringSilkRoad/Data/SoftwareEngineeringSilkRoadContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SoftwareEngineeringSilkRoad.Models;
+
+namespace SoftwareEngineeringSilkRoad.Data
+{
+    public class SoftwareEngineeringSilkRoadContext : DbContext
+    {
+        public SoftwareEngineeringSilkRoadContext (DbContextOptions<SoftwareEngineeringSilkRoadContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SoftwareEngineeringSilkRoad.Models.Customer> Customer { get; set; }
+
+        public DbSet<SoftwareEngineeringSilkRoad.Models.Product> Product { get; set; }
+
+        public DbSet<SoftwareEngineeringSilkRoad.Models.ShoppingCart> ShoppingCart { get; set; }
+
+        public DbSet<SoftwareEngineeringSilkRoad.Models.ProductReview> ProductReview { get; set; }
+
+        public DbSet<SoftwareEngineeringSilkRoad.Models.WishList> WishList { get; set; }
+    }
+}
