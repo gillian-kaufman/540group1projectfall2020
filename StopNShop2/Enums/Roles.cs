e19e48cdb70682b416eedd94292758514022b743
auzman466 <auzman446@gmail.com>
2020-10-12 10:53:04 -0400

    Fixed applicationUser

---
+++ StopNShop2/Enums/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StopNShop2.Enums
+{
+    public enum Roles
+    {
+        Admin,
+        User
+    }
+}
---
+++ StopNShop2/Models/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace StopNShop2.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        public int ZipCode { get; set; }
+    }
+}
